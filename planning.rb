fdf88daa4ab655e53f71a4f361e30649e7889802
Taylor Noj <taylornoj@gmail.com>
2022-01-01 22:04:12 +0000

    planning structure of classes

---
+++ planning.rb
@@ -0,0 +1,9 @@
+## extract nouns for classes:
+  # game > houses all the main code; gives structure to the game itself (behaviour)
+    # score - turn - winner
+  # player > keeps players organized (p1, p2) with # of lives they have (state)
+    # name - lives 
+  # question > formatting the question and setting up how the answer is calculated (state)
+    # new question - answer
+
+
