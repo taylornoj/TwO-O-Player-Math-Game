82438aa3d228f06a657a1a7a658a8a6fd2cac50e
Taylor Noj <taylornoj@gmail.com>
2022-01-03 20:31:34 +0000

    set up game and question classes with methods

--- game.rb
+++ game.rb
@@ -1,3 +1,25 @@
 require './players'
 require './question'
 
+class Game
+  def initialize
+    @player1
+    @player2
+    @current_player
+  end
+
+
+  def turn
+  end
+
+  def start_game
+  end
+
+
+  def score
+  end
+
+
+
+
+end
--- question.rb
+++ question.rb
@@ -1,13 +1,13 @@
 class Question
 
   def initialize
-    @num1 = 
-    @num2 = 
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
   
   end
 
-  def new_question
-    
+  def new_question(name)
+    puts "#{name}: What does #{num1} plus #{num2} equal?"
   end
 
   def sum
