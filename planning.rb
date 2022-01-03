0ec41003e725abc827f66cd1c95ab35a6be88d1e
Taylor Noj <taylornoj@gmail.com>
2022-01-03 17:20:01 +0000

    update planning

--- planning.rb
+++ planning.rb
@@ -2,14 +2,15 @@
   # game > houses all the main code; gives structure to the game itself (behaviour)
     # score - turn - winner
     # contains game loop
-    
-  # player > keeps players organized with # of lives they have (state)
-    # name - lives 
     # manages current player
+    
+  # player > initializes players by name and lives (state)
+    # name - lives 
 
   # question > formatting the question and setting up how the answer is calculated (state)
     # new_question,  sum
       # initialize number1 and number2 for the question
+      # dictate what answer should be with sum
 
 
 # user I/O?
