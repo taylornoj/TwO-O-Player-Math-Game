2e31c7d0b922f02f5f88da679435320c1147d3e3
Taylor Noj <taylornoj@gmail.com>
2022-01-03 21:56:48 +0000

    update question class

--- question.rb
+++ question.rb
@@ -1,21 +1,10 @@
-
-
 class Question
   attr_accessor :num1, :num2
 
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
+    @solution = @num1 + @num2
   
   end
-
-  def new_question(name)
-    puts "#{name}: What does #{@num1} plus #{@num2} equal?"
-  end
-
-  def sum
-    @num1 + @num2
-  end
-
-
 end
