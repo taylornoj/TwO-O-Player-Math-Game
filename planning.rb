ba64aeb837fae45b8301870da04160c9ba086fc7
Taylor Noj <taylornoj@gmail.com>
2022-01-03 16:21:20 +0000

    update planning and create class files

--- planning.rb
+++ planning.rb
@@ -1,9 +1,15 @@
 ## extract nouns for classes:
   # game > houses all the main code; gives structure to the game itself (behaviour)
     # score - turn - winner
-  # player > keeps players organized (p1, p2) with # of lives they have (state)
+    # contains game loop
+    
+  # player > keeps players organized with # of lives they have (state)
     # name - lives 
+    # manages current player
+
   # question > formatting the question and setting up how the answer is calculated (state)
-    # new question - answer
+    # new_question,  sum
+      # initialize number1 and number2 for the question
 
 
+# user I/O?
