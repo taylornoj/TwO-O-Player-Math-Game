94af2473c06e32b29cbe3ecd964036c6424b7dac
Taylor Noj <taylornoj@gmail.com>
2022-01-03 22:22:41 +0000

    correct score reflecting on correct player

--- game.rb
+++ game.rb
@@ -16,26 +16,51 @@ class Game
     # end
 
 
-  def turn
-    if @current_player == @player1
-      @current_player = @player2
-    else
+    
+    def start_game
+      puts "----- TwO-O-Player -----"
+      puts "\n"
+      puts "Player 1: Please enter your name"
+      @player1 = Player.new(gets.chomp)
+      
+      puts "Player 2: Please enter your name"
+      @player2 = Player.new(gets.chomp)
+      
+      puts "Let's play!"
+      puts "\n"
       @current_player = @player1
+      
+      game_play while (@player1.lives > 0 && @player2.lives > 0)
+      end_game
     end
-  end
 
-  def start_game
-    puts "----- TwO-O-Player -----"
+    def turn
+      if @current_player == @player1
+        @current_player = @player2
+      else
+        @current_player = @player1
+      end
+    end
+
+  def game_play
+    question = Question.new
     puts "\n"
-    puts "Player 1: Please enter your name"
-    @player1 = Player.new(gets.chomp)
-
-    puts "Player 2: Please enter your name"
-    @player2 = Player.new(gets.chomp)
-
-    puts "Let's play!"
+    puts "----- NEW TURN -----"
     puts "\n"
+    puts "#{@current_player.name}: What does #{question.num1} plus #{question.num2} equal?"
+    answer = gets.chomp.to_i
 
+    if answer == question.solution
+      puts "\n"
+      puts "YES! You are correct!"
+    else
+      puts "\n"
+      puts "WRONGO!"
+      @current_player.lives -= 1
+      puts "\n"
+    end
+    puts "SCORE: #{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+    turn
   end
 
 
