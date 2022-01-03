12701675b3f1f1a3a156351b43f6eef8d07139a7
Taylor Noj <taylornoj@gmail.com>
2022-01-03 22:06:15 +0000

    reworking entire game to break down game components better

--- game.rb
+++ game.rb
@@ -3,59 +3,76 @@ require "./question"
 
 class Game
   def initialize
-    @player1 = Player.new('Taylor', 3)
-    @player2 = Player.new('Louis', 3)
+    @player1 = Player.new('Taylor')
+    @player2 = Player.new('Louis')
     @current_player = @player1
   end
   
-    def score
-      puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
-      puts "\n"
-      puts "----- NEW TURN -----"
-      puts "\n"
-    end
+    # def score
+    #   puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+    #   puts "\n"
+    #   puts "----- NEW TURN -----"
+    #   puts "\n"
+    # end
 
 
   def turn
-    @current_player = (@current_player == @player1) ? @player2 : @player1
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
   end
 
   def start_game
     puts "----- TwO-O-Player -----"
     puts "\n"
-    self.turn
-    question = Question.new
-    question.new_question(@current_player.name)
-    answer = gets.chomp.to_i
+    puts "Player 1: Please enter your name"
+    @player1 = Player.new(gets.chomp)
 
-    if answer == question.sum
-      puts "YES! You are correct!"
-      self.score
-      self.turn
+    puts "Player 2: Please enter your name"
+    @player2 = Player.new(gets.chomp)
 
-
-    else
-      puts "Seriously? No!"
-      @current_player.lives -= 1
-      self.score
-      if(@current_player.lives > 0)
-        self.start_game
-      else
-        puts "Game Over"
-        self.turn
-        puts "#{@current_player.name} wins with a score of #{@current_player.lives}"
-      end
-
-    end
-
-
-
-
-
-
+    puts "Let's play!"
+    puts "\n"
 
   end
 
+
+
+
+    # self.turn
+    # question = Question.new
+    # question.new_question(@current_player.name)
+    # answer = gets.chomp.to_i
+
+    # if answer == question.sum
+    #   puts "YES! You are correct!"
+    #   self.score
+    #   self.turn
+
+
+    # else
+    #   puts "Seriously? No!"
+    #   @current_player.lives -= 1
+    #   self.score
+    #   if(@current_player.lives > 0)
+    #     self.start_game
+    #   else
+    #     puts "Game Over"
+    #     self.turn
+    #     puts "#{@current_player.name} wins with a score of #{@current_player.lives}"
+    #   end
+
+    
+
+
+
+
+
+
+
+  
 
 
 
